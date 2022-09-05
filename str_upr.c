977490c66a2422d366ed69cbd861d853a0f92ad9
Kavitha-52 <98574222+Kavitha-52@users.noreply.github.com>
2022-09-05 21:45:05 +0530

    string  upper

---
+++ str_upr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+        char a[]="hello";
+        int i=0;
+        while(a[i]!='\0')
+        {
+        if(a[i]>='a' && a[i]<='z')
+        {
+                a[i]=a[i]-32;
+        }
+        i++;
+        }
+        printf("string upper:%s",a);
+}
