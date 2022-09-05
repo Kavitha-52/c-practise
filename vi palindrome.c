cdfdc300ef537d1387ec6439422ebee5de4d8143
Kavitha-52 <98574222+Kavitha-52@users.noreply.github.com>
2022-09-05 21:23:02 +0530

    string is palindrome  or not

---
+++ vi palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+        char a[]="radar";
+        int i=0,j=0;
+        while(a[j]!='\0')
+        {
+                j++;
+        }
+        j=j-1;
+        while(i<j)
+        {
+                if(a[i]!=a[j])
+                        break;
+                i++;
+                j--;
+                {  
+      if(i>=j)
+                printf("string is palindrome\n");
+        else
+                printf("string is not palindrome\n");
+}
